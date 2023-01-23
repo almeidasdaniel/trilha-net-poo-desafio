b8a975c59802376316ae1574236535246b05ecf0
almeidasdaniel <119354267+almeidasdaniel@users.noreply.github.com>
2023-01-23 17:38:45 -0300

    Alteração Program
    Alteração para testar a implementação das classes Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+
+var nokia = new Nokia(numero: "111222", modelo: "novo modelo", imei: "1111111", memoria:128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Zoom");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone:");
+
+var iphone = new Iphone(numero: "111222", modelo: "13", imei: "1111122", memoria:128);
+iphone.Ligar();
+iphone.InstalarAplicativo("YouTube");
